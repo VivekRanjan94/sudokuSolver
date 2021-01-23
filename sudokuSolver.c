1f6df5a8dded800248c00ecf8e0f2f0f0cdab1d9
Vivek <vivek.ranjaan@gmail.com>
2021-01-23 13:40:38 +0530

    Completed logic

--- sudokuSolver.c
+++ sudokuSolver.c
@@ -17,6 +17,7 @@ void update(int, int, int);
 void solveEasy();
 void solveMed();
 void solveHard();
+void solveExpert();
 void solveBoard();
 
 //Main function
@@ -39,7 +40,24 @@ int main()
     printf("\n");
 
     //Solving sudoku board
-    solveBoard();
+    int previousCheck = 0;
+    for (;;)
+    {
+        solveBoard();
+        if (!checkSolve())
+        {
+            break;
+        }
+        else if (checkSolve() == previousCheck)
+        {
+            solveExpert();
+            if (checkSolve() == previousCheck)
+            {
+                break;
+            }
+        }
+        previousCheck = checkSolve();
+    }
 
     //printing solved sudoku board
     printBoard();
@@ -322,9 +340,7 @@ void solveHard()
             }
         }
     }
-    //rowNum = 5
-    //colNum = 0
-    // number = 1
+
     for (int colNum = 0; colNum < 9; colNum++)
     {
         for (int rowNum = 0; rowNum < 9; rowNum++)
@@ -370,10 +386,203 @@ void solveHard()
     }
 }
 
+void solveExpert()
+{
+    int possibilities[9][9][9];
+    int possibleFill[81];
+    int iterator = 0;
+    //possibleFill[i] is 4 digit number whose thousands digit is rowNum
+    //hundreds digit is colNum, tens digit is minNumber, and units digit is minCount
+
+    //initialize possibleFill[81]
+    for (int i = 0; i < 81; i++)
+    {
+        possibleFill[i] = 9999;
+    }
+
+    //Board1 stores a copy of board
+    int board1[9][9];
+    for (int rowNum = 0; rowNum < 9; rowNum++)
+    {
+        for (int colNum = 0; colNum < 9; colNum++)
+        {
+            board1[rowNum][colNum] = board[rowNum][colNum];
+        }
+    }
+
+    //Initialize possibilities
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            for (int k = 0; k < 9; k++)
+            {
+                possibilities[i][j][k] = 0;
+            }
+        }
+    }
+
+    //Set all possibilities
+    for (int rowNum = 0; rowNum < 9; rowNum++)
+    {
+        for (int colNum = 0; colNum < 9; colNum++)
+        {
+            if (!board[rowNum][colNum])
+            {
+                for (int number = 1; number <= 9; number++)
+                {
+                    if (!locations[number - 1][0][rowNum] &&
+                        !locations[number - 1][1][colNum] &&
+                        !locations[number - 1][2][square(rowNum, colNum)])
+                    {
+                        possibilities[rowNum][colNum][number - 1] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    //Initialize twoPossibilities
+    int twoPossibilities[9][9][2];
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                twoPossibilities[i][j][k] = 0;
+            }
+        }
+    }
+
+    //Count possibilities and if count == 2 store it in twoPossibilities
+    //Set twopossibilities
+    for (int rowNum = 0; rowNum < 9; rowNum++)
+    {
+        for (int colNum = 0; colNum < 9; colNum++)
+        {
+            if (!board[rowNum][colNum])
+            {
+                int count = 0;
+                for (int number = 1; number <= 9; number++)
+                {
+                    if (possibilities[rowNum][colNum][number - 1])
+                    {
+                        twoPossibilities[rowNum][colNum][count] = number;
+                        count++;
+                    }
+                    if (count > 2)
+                    {
+                        twoPossibilities[rowNum][colNum][0] = 0;
+                        twoPossibilities[rowNum][colNum][1] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    //Now we have possibilities at (i,j) stored in possibilities[i][j][]
+    //All elements with only two possibilities stored in twoPossiblities[i][j][]
+    //These possibilities are stored
+
+    for (int rowNum = 0; rowNum < 9; rowNum++)
+    {
+        for (int colNum = 0; colNum < 9; colNum++)
+        {
+            if (!board[rowNum][colNum])
+            {
+                int countP1 = 0;
+                int countP2 = 0;
+                for (int rowNum2 = 0; rowNum2 < 9; rowNum2++)
+                {
+                    if (twoPossibilities[rowNum][colNum][0] && rowNum2 != rowNum)
+                    {
+                        if (possibilities[rowNum2][colNum][twoPossibilities[rowNum][colNum][0] - 1])
+                        {
+                            countP1++;
+                        }
+                        if (possibilities[rowNum2][colNum][twoPossibilities[rowNum][colNum][1] - 1])
+                        {
+                            countP2++;
+                        }
+                    }
+                }
+                for (int colNum2; colNum2 < 9; colNum2++)
+                {
+
+                    if (twoPossibilities[rowNum][colNum][0] && colNum2 != colNum)
+                    {
+                        if (possibilities[rowNum][colNum2][twoPossibilities[rowNum][colNum][0] - 1])
+                        {
+                            countP1++;
+                        }
+                        if (possibilities[rowNum][colNum2][twoPossibilities[rowNum][colNum][1] - 1])
+                        {
+                            countP2++;
+                        }
+                    }
+                }
+
+                if (countP1 > countP2)
+                {
+                    possibleFill[iterator] = rowNum * 1000;
+                    possibleFill[iterator] += colNum * 100;
+                    possibleFill[iterator] += twoPossibilities[rowNum][colNum][0] * 10;
+                    possibleFill[iterator] += countP2;
+                    iterator++;
+                }
+                else if (countP1 < countP2)
+                {
+                    possibleFill[iterator] = rowNum * 1000;
+                    possibleFill[iterator] += colNum * 100;
+                    possibleFill[iterator] += twoPossibilities[rowNum][colNum][0] * 10;
+                    possibleFill[iterator] += countP1;
+                    iterator++;
+                }
+            }
+        }
+    }
+    //put the next lines in for loop of sorted array
+    //sort array possibleFill
+    for (int i = 1; i < iterator; i++)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            if (possibleFill[j + 1] % 10 < possibleFill[j] % 10)
+            {
+                int temp = possibleFill[j];
+                possibleFill[j] = possibleFill[j + 1];
+                possibleFill[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < iterator; i++)
+    {
+        update(possibleFill[i] / 1000, (possibleFill[i] % 1000) / 100, (possibleFill[i] % 100) / 10);
+        solveBoard();
+        if (checkSolve())
+        {
+            for (int rowNum = 0; rowNum < 9; rowNum++)
+            {
+                for (int colNum = 0; colNum < 9; colNum++)
+                {
+                    board[rowNum][colNum] = board1[rowNum][colNum];
+                }
+            }
+            locationsInit();
+            setLocations();
+        }
+        else
+        {
+            return;
+        }
+    }
+}
+
 void solveBoard()
 {
     int previousCheck = 0;
-    int loopCount = 0;
     for (;;)
     {
         if ((!checkSolve()) || (checkSolve() == previousCheck))
@@ -388,7 +597,5 @@ void solveBoard()
         previousCheck = checkSolve();
         solveEasy();
         solveMed();
-        loopCount++;
     }
-    printf("%d\n", loopCount);
 }
