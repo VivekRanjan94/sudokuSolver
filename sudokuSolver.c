a5cf67acfe13a4ba2856522855c995f537fdaad1
Vivek <vivek.ranjaan@gmail.com>
2021-01-24 20:02:16 +0530

    Fixed completely wrong extreme in expert function

--- sudokuSolver.c
+++ sudokuSolver.c
@@ -35,7 +35,7 @@ int main()
     //Printing sudoku board to solve
     printBoard();
 
-    printf("\n\nEnter any key to solve the board:");
+    printf("\n\nEnter any key to solve the board:\n");
     getchar();
 
     //Solving sudoku board
@@ -412,8 +412,9 @@ void solveExpert()
     int possibilities[9][9][9];
     //possibilities[9][9][9] is an array that stores whether a number can be filled in an element
     //possibilities[i][j][k-1] means that the number k can be filled in the coordinates (i,j)
+
     int possibleFill[81];
-    //possibleFill[i] is 4 digit number whose thousands digit is rowNum
+    //possibleFill[i] is an array of 4 digit number whose thousands digit is rowNum
     //hundreds digit is colNum, tens digit is minNumber, and units digit is minCount
     int iterator = 0;
 
@@ -561,7 +562,7 @@ void solveExpert()
                 {
                     possibleFill[iterator] = rowNum * 1000;
                     possibleFill[iterator] += colNum * 100;
-                    possibleFill[iterator] += twoPossibilities[rowNum][colNum][0] * 10;
+                    possibleFill[iterator] += twoPossibilities[rowNum][colNum][1] * 10;
                     possibleFill[iterator] += countP2;
                     iterator++;
                 }
@@ -586,6 +587,8 @@ void solveExpert()
         If all values of possibleFill are checked and none can solve the sudoku board, then a boolean value extreme is set to true
         If extreme == true, then instead of trying one possibility from possibleFill[81] we try 2 possiblities simultaneously
     */
+
+    //    Insertion sort for array possibleFill
     for (int i = 1; i < iterator; i++)
     {
         for (int j = i; j >= 0; j--)
@@ -627,22 +630,201 @@ void solveExpert()
     {
         for (int i = 0; i < iterator; i++)
         {
+            int iterator2 = 0;
             update(possibleFill[i] / 1000, (possibleFill[i] % 1000) / 100, (possibleFill[i] % 100) / 10);
-            for (int j = i + 1; j < iterator; j++)
+            solveBoard();
+            //Update newTwoPossibilities array according to new board after filling possibleFill[i]
+
+            int newPossibilities[9][9][9];
+            //Initialize newPossibilities[][][]
+            for (int i = 0; i < 9; i++)
             {
-                update(possibleFill[j] / 1000, (possibleFill[j] % 1000) / 100, (possibleFill[j] % 100) / 10);
+                for (int j = 0; j < 9; j++)
+                {
+                    for (int k = 0; k < 9; k++)
+                    {
+                        newPossibilities[i][j][k] = 0;
+                    }
+                }
+            }
+            //Set all newPossibilities
+            for (int rowNum = 0; rowNum < 9; rowNum++)
+            {
+                for (int colNum = 0; colNum < 9; colNum++)
+                {
+                    if (!board[rowNum][colNum])
+                    {
+                        for (int number = 1; number <= 9; number++)
+                        {
+                            if (!locations[number - 1][0][rowNum] &&
+                                !locations[number - 1][1][colNum] &&
+                                !locations[number - 1][2][square(rowNum, colNum)])
+                            {
+                                newPossibilities[rowNum][colNum][number - 1] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            int newPossibleFill[81];
+            //initialize newPossibleFill[81]
+            for (int l = 0; l < 81; l++)
+            {
+                newPossibleFill[l] = 9999;
+            }
+            int newTwoPossibilities[9][9][2];
+            // newTwoPossibilities[9][9][2] stores which coordinates only have 2 possible numbers that can be filled in them
+            // newTwoPossibilities[i][j][0] gives the first possibility that can be stored in (i,j) and newTwoPossibilities[i][j][1] returns the other
+
+            //Initialize newTwoPossibilities
+            for (int l = 0; l < 9; l++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        newTwoPossibilities[l][j][k] = 0;
+                    }
+                }
+            }
+
+            //Count newPossibilities and if count == 2 store it in newTwoPossibilities
+            //Set newTwoPossibilities
+            for (int rowNum = 0; rowNum < 9; rowNum++)
+            {
+                for (int colNum = 0; colNum < 9; colNum++)
+                {
+                    if (!board[rowNum][colNum])
+                    {
+                        int count = 0;
+                        for (int number = 1; number <= 9; number++)
+                        {
+                            if (newPossibilities[rowNum][colNum][number - 1])
+                            {
+                                if (count == 2)
+                                {
+                                    newTwoPossibilities[rowNum][colNum][0] = 0;
+                                    newTwoPossibilities[rowNum][colNum][1] = 0;
+                                    break;
+                                }
+                                newTwoPossibilities[rowNum][colNum][count] = number;
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            /*
+                Now we have newPossibilities at (i,j) stored in newPossibilities[i][j][k]
+                All elements with only two newPossibilities stored in twoPossiblities[i][j]
+
+
+                Now the number of times a possibility appears in that respective row and column is stored in countP1 and countP2
+                The minimum of these will be stored in newPossibleFill[i] as rowNum*1000 + colNum*100 + minValue*10 + min
+                where min is the minimum of countP1 and countP2 and minValue is the respective number to be filled
+            */
+            for (int rowNum = 0; rowNum < 9; rowNum++)
+            {
+                for (int colNum = 0; colNum < 9; colNum++)
+                {
+                    if (!board[rowNum][colNum])
+                    {
+                        int countP1 = 0;
+                        int countP2 = 0;
+                        for (int rowNum2 = 0; rowNum2 < 9; rowNum2++)
+                        {
+                            if (newTwoPossibilities[rowNum][colNum][0] && rowNum2 != rowNum)
+                            {
+                                if (newPossibilities[rowNum2][colNum][newTwoPossibilities[rowNum][colNum][0] - 1])
+                                {
+                                    countP1++;
+                                }
+                                if (newPossibilities[rowNum2][colNum][newTwoPossibilities[rowNum][colNum][1] - 1])
+                                {
+                                    countP2++;
+                                }
+                            }
+                        }
+                        for (int colNum2; colNum2 < 9; colNum2++)
+                        {
+
+                            if (newTwoPossibilities[rowNum][colNum][0] && colNum2 != colNum)
+                            {
+                                if (newPossibilities[rowNum][colNum2][newTwoPossibilities[rowNum][colNum][0] - 1])
+                                {
+                                    countP1++;
+                                }
+                                if (newPossibilities[rowNum][colNum2][newTwoPossibilities[rowNum][colNum][1] - 1])
+                                {
+                                    countP2++;
+                                }
+                            }
+                        }
+
+                        if (countP1 > countP2)
+                        {
+                            newPossibleFill[iterator2] = rowNum * 1000;
+                            newPossibleFill[iterator2] += colNum * 100;
+                            newPossibleFill[iterator2] += newTwoPossibilities[rowNum][colNum][1] * 10;
+                            newPossibleFill[iterator2] += countP2;
+                            iterator2++;
+                        }
+                        else if (countP1 < countP2)
+                        {
+                            newPossibleFill[iterator2] = rowNum * 1000;
+                            newPossibleFill[iterator2] += colNum * 100;
+                            newPossibleFill[iterator2] += newTwoPossibilities[rowNum][colNum][0] * 10;
+                            newPossibleFill[iterator2] += countP1;
+                            iterator2++;
+                        }
+                    }
+                }
+            }
+
+            /*
+                Array newPossibleFill[81] is sorted by ascending order of min using insertion sort
+                Here min is the minimum number of times the possibility checked appears in that row and column
+                Now the possibility with least min will be filled into board and we will use solveBoard()
+                If the sudoku board remains unsolved after this, then it will reset board back to the state it was in 
+                using board1 and then try the next item in newPossibleFill[81]
+                If all values of newPossibleFill are checked and none can solve the sudoku board, then a boolean value extreme is set to true
+                If extreme == true, then instead of trying one possibility from newPossibleFill[81] we try 2 possiblities simultaneously
+            */
+
+            //    Insertion sort for array newPossibleFill
+            for (int l = 1; l < iterator2; l++)
+            {
+                for (int j = l; j >= 0; j--)
+                {
+                    if (newPossibleFill[j + 1] % 10 < newPossibleFill[j] % 10)
+                    {
+                        int temp = newPossibleFill[j];
+                        newPossibleFill[j] = newPossibleFill[j + 1];
+                        newPossibleFill[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int j = 0; j < iterator2; j++)
+            {
+                update(newPossibleFill[j] / 1000, (newPossibleFill[j] % 1000) / 100, (newPossibleFill[j] % 100) / 10);
                 solveBoard();
                 if (checkSolve())
                 {
-                    if (i == iterator - 1)
-                    {
-                        extreme = true;
-                    }
                     for (int rowNum = 0; rowNum < 9; rowNum++)
                     {
                         for (int colNum = 0; colNum < 9; colNum++)
                         {
-                            board[rowNum][colNum] = board1[rowNum][colNum];
+                            if ((possibleFill[i]) / 1000 == rowNum && (possibleFill[i] % 1000) / 100 == colNum)
+                            {
+                                board[rowNum][colNum] = (possibleFill[i] % 100) / 10;
+                            }
+                            else
+                            {
+                                board[rowNum][colNum] = board1[rowNum][colNum];
+                            }
                         }
                     }
                     locationsInit();
